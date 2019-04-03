190c139cc87e17f923c2404b75d743dff1d2dee8
Earl Platt III <36335481+eaplatt3@users.noreply.github.com>
2019-04-03 11:42:54 -0400

    Initial commit

---
+++ SubroutineMath.pl
@@ -0,0 +1,14 @@
+####################################
+# Earl Platt III
+# 4/3/2019
+# BCS 316 - Perl Programming
+# Subroutine Math
+# Input - User Enters Numbers
+# Output - Math Done with Numbers 
+####################################
+
+use strict;
+use warnings;
+
+my $num1;
+my $num2; 
